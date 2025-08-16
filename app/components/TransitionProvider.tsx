b871d9ce0e470b0c0e6eff6b2d5f9e9903db611d
Ali <ali0fawzish@outlook.com>
2025-08-16 23:14:20 +0300

    refactor: Simplify TransitionProvider by removing unnecessary animation logic

--- app/components/TransitionProvider.tsx
+++ app/components/TransitionProvider.tsx
@@ -1,19 +1,7 @@
-"use client";
-
-import { AnimatePresence } from "framer-motion";
-import { usePathname } from "next/navigation";
-import Transition from "./Transition";
-
 export default function TransitionProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
-  return (
-    <AnimatePresence mode="wait" initial={false}>
-      <Transition key={"pathname"}>{children}</Transition>
-    </AnimatePresence>
-  );
+  return <>{children}</>;
 }
